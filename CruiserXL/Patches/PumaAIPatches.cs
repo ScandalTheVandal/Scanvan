8dc97eae084b1b1c54ea77f5e99d3590743bcae7
ScandalTheVandal <saab99turboman@gmail.com>
2026-04-10 03:03:48 +0100

    Update Puma protection (v81)

--- CruiserXL/Patches/PumaAIPatches.cs
+++ CruiserXL/Patches/PumaAIPatches.cs
@@ -10,33 +10,28 @@ internal static class PumaAIPatches
 {
     [HarmonyPatch(nameof(PumaAI.OnCollideWithPlayer))]
     [HarmonyPrefix]
-    static void OnCollideWithPlayer_Prefix(PumaAI __instance)
+    static bool OnCollideWithPlayer_Prefix(PumaAI __instance, Collider other)
     {
-        PlayerControllerB playerControllerB = GameNetworkManager.Instance.localPlayerController;
+        PlayerControllerB playerControllerB = __instance.MeetsStandardPlayerCollisionConditions(other, false, false);
 
-        if (playerControllerB == null || 
-            !playerControllerB.isPlayerControlled || 
-            playerControllerB.isPlayerDead)
-            return;
+        if (playerControllerB == null)
+            return true;
 
         if (References.truckController == null)
-            return;
+            return true;
         CruiserXLController controller = References.truckController;
         var avgSpeed = controller.averageVelocity.magnitude;
 
         // check if the player is seated in our truck
         if (VehicleUtils.IsPlayerSeatedInVehicle(controller))
         {
-            // reset the timer, to prevent the puma from damaging the player, i guess
-            if (avgSpeed < 10f && VehicleUtils.IsSeatedPlayerProtected(playerControllerB, controller))
-            {
-                __instance.timeAtLastScratch = Time.realtimeSinceStartup;
-                return;
-            }
-            else if (avgSpeed >= 10f)
-            {
-                __instance.timeAtLastScratch = Time.realtimeSinceStartup;
-            }
+            if (avgSpeed >= 2f)
+                return false;
+
+            // player is protected, so do not allow the kill
+            if (VehicleUtils.IsSeatedPlayerProtected(playerControllerB, controller))
+                return false;
+            return true; // allow vanilla logic to run
         }
 
         bool enemyInTruck = VehicleUtils.IsEnemyInVehicle(__instance, controller);
@@ -44,24 +39,30 @@ internal static class PumaAIPatches
         {
             // enemy is not in the back with the player
             if (PlayerUtils.isPlayerInStorage && !enemyInTruck)
-                __instance.timeAtLastScratch = 4f;
+                return false;
+
+            // player is just riding on the truck
+            if (!PlayerUtils.isPlayerInStorage && !PlayerUtils.isPlayerInCab)
+                return avgSpeed < 2f;
 
             // player is standing in the cab
             if (PlayerUtils.isPlayerInCab)
             {
-                if (avgSpeed >= 10f ||
-                    (!controller.driverSideDoor.boolValue && !controller.passengerSideDoor.boolValue && 
-                    !controller.driversSideWindowTrigger.boolValue && !controller.passengersSideWindowTrigger.boolValue))
+                // vehicle is going slow and either side door is open
+                if (avgSpeed < 2f &&
+                    (controller.driverSideDoor.boolValue || controller.passengerSideDoor.boolValue))
                 {
-                    __instance.timeAtLastScratch = Time.realtimeSinceStartup;
+                    return true;
                 }
+                return false;
             }
+            return true;
         }
         else
         {
-            // reset the timer, to prevent the puma from damaging the player
             if (enemyInTruck)
-                __instance.timeAtLastScratch = Time.realtimeSinceStartup;
+                return false;
         }
+        return true; // run vanilla logic
     }
 }
